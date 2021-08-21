7c3d4bab438d8ae11bea60a856576ef0805d7615
Leo Rawlinson <leo.m.rawlinson@gmail.com>
2021-08-21 15:50:26 +0200

    Itt one complete

--- routes/drones.js
+++ routes/drones.js
@@ -1,34 +1,43 @@
-const express = require('express');
+const express = require("express");
+const Drone = require("../models/Drone.model");
 const router = express.Router();
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  Drone.find()
+    .then((allDrones) => {
+      console.log("Here are all your drones", allDrones);
+      res.render("drones/list", { allDrones });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get("/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,34 @@
 // Iteration #1
+//connect to the database and seed the documents in it, in this case books
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+// ℹ️ Connects to the database
+require("../db");
+
+const drones = [
+  {
+    name: "UFO",
+    propellers: 4,
+    maxSpeed: 20,
+  },
+  {
+    name: "Red",
+    propellers: 2,
+    maxSpeed: 15,
+  },
+  {
+    name: "Banana",
+    propellers: 3,
+    maxSpeed: 18,
+  },
+];
+
+Drone.create(drones)
+  .then((createdDrones) => {
+    console.log(`Created ${createdDrones.length} drones`);
+
+    mongoose.connection.close();
+  })
+  .catch((error) =>
+    console.log("An Error ocurred on the book creation", error)
+  );
